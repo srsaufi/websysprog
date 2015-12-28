38aef60e0cbb21e815051878f514f46642fdfb43
Saufi Rahman <Saufi Rahman>
2015-12-28 13:47:24 +0700

    Tambahn

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 	<link href="css/style.css" rel="stylesheet">
 	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100' rel='stylesheet' type='text/css'>
 	<title>Web Service WATER FLOW DETECTOR</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 </head>
 <body>
 
@@ -32,18 +33,18 @@
 			<center><h3>Machine Status</h3></center>
 			<hr>
 
-			<center><button class="btn" id="onoff" onclick="onoff()"><img src="img/power-button.gif" height="100" width="100" ></button></center>
+			<center><button class="btn" id="onoff" onclick="loadJSON()" ><img src="img/power-button.gif" height="100" width="100" ></button></center>
 			<br>
 			<center><p>Status : </p>
-				<div id="status"><?php echo $status; ?></div>
+				<div id="status"></div>
 			</center>
-		<br><br><br><br>
+			<br><br><br><br>
 		</div>
 
 
 		<div class="borderleft col-md-4">
-		<br><br><br><br>
-				<div class="list-group">
+			<br><br><br><br>
+			<div class="list-group">
 				<a href="#" class="list-group-item disabled">
 					DETAILS
 				</a>
@@ -56,11 +57,11 @@
 	
 	<div class=" background-grey container">
 		<br><br>
-		<h1 style="margin-left:15px;"> Progres Bar Still 70% :</h1>
+		<h1 style="margin-left:15px;" id="levelText"></h1>
 		<div class="col-md-12">
 			<div class="progress">
-				<div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%">
-					<span class="sr-only">70% Complete (success)</span>
+				<div id="levelWater" class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="" aria-valuemin="0" aria-valuemax="100" style="width: 70%">
+					<span id="leve" class="sr-only"></span>
 				</div>
 			</div>
 		</div>
@@ -68,20 +69,41 @@
 	</div>
 
 	<script>
-			function onoff() {
 
-				 <?php
-				 if($status == "OFF"){
-				 	$status = "ON";
-				 }else{
-				 	$status = "OFF";
-				 }
+		$(document ).ready(function(){
+			var myVar = setInterval(levelWaterUpdate ,1000);
+			function levelWaterUpdate(){
+				var xmlhttp = new XMLHttpRequest();
+				var url = "http://localhost/sysprogweb/data.json";
 
-				 ?>
+				xmlhttp.onreadystatechange=function() {
+					if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+						myFunction(xmlhttp.responseText);
+					}
+				}
+				xmlhttp.open("GET", url, true);
+				xmlhttp.send();
 				
+				function myFunction(response) {
+					var arr = JSON.parse(response);
+					if(arr.Level >= 200){
+						alert('FULL');
+						clearTimeout(myVar);
+					}
+					var level = (arr.Level / 200) *100;
+					document.getElementById("levelWater").setAttribute("aria-valuenow", level);
+					document.getElementById("levelWater").setAttribute("style","width :" + level+  "%" );
+					document.getElementById("levelText").innerHTML = level + "%";
+				}
 
 			}
-		</script>
 
-	</body>
-	</html>
+
+		});
+
+
+
+	</script>
+
+</body>
+</html>
