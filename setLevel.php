80530c915e26bcb9e8b4d8ae80222711d80e1497
rxsau <sr.saufi@gmail.com>
2015-12-29 05:40:35 +0700

    untuk post metod set Level json format {"status":3} 3cm

---
+++ setLevel.php
@@ -0,0 +1,11 @@
+<?php
+
+if(isset($_POST['data']) && !empty($_POST['data'])) {
+	
+	$data = json_decode(stripslashes($_POST['data']));
+	$myfile = fopen("data.json", "w") or die("Unable to open file!");
+	fwrite($myfile, $_POST['data']);
+	fclose($myfile);
+}
+
+?>
